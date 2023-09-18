2b3a1bffc081083fcbdf84130ee01eb8937b557c
bnasare <124492150+bnasare@users.noreply.github.com>
2023-09-18 19:29:54 +0000

    learning classes

---
+++ classes.py
@@ -0,0 +1,15 @@
+class Cookies:
+    def __init__(self, color):
+        self.color = color
+    def get_color(self):
+        return self.color
+    def set_color(self, color):
+        self.color = color
+
+cookies_one = Cookies('red')
+cookies_two = Cookies('red')
+
+cookies_one.set_color('yellow')
+
+print('I love', cookies_two.get_color())
+print('I love', cookies_one.get_color())
